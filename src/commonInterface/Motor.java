72ed2cb6c37a3c75658f74c11dd2e00a27baeb3f
PaulBreugnot <paul.breugnot@etu.emse.fr>
2017-11-19 10:36:15 +0100

    Implementing commonInterface basic architecture

---
+++ src/commonInterface/Motor.java
@@ -0,0 +1,6 @@
+package commonInterface;
+
+//This interface needs to be implemented by the Simulation program and the real robot control program.
+public interface Motor {
+
+}
--- src/commonInterface/Robot.java
+++ src/commonInterface/Robot.java
@@ -1,5 +1,9 @@
 package commonInterface;
 
+import java.util.ArrayList;
+
 public class Robot {
+	private ArrayList<Motor> robotMotors = new ArrayList<>();
+	private ArrayList<Captor> robotCaptors = new ArrayList<>();
 
 }
